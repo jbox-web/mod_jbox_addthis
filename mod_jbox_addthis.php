3279274b962f2cc6961834c99860e02f60e04a8e
Lior <lchamla@gmail.com>
2014-02-16 15:07:20 +0100

    fix copyright, comments and dates

--- mod_jbox_addthis.php
+++ mod_jbox_addthis.php
@@ -4,7 +4,7 @@
  * @author      Lior Chamla (lchamla@jbox-web.com)
  * @abstract    Adds the AddThis SmartLayer to Joomla!
  * @subpackage  mod_jbox_addthis
- * @copyright   Copyright (C) 20011 - 2014 JBox Web (http://www.jbox-web.com).
+ * @copyright   Copyright (C) 2014 JBox Web (http://www.jbox-web.com).
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
--- script.php
+++ script.php
@@ -4,13 +4,20 @@
  * @author 		Lior Chamla (lchamla@jbox-web.com)
  * @abstract	Adds the AddThis SmartLayer to Joomla!
  * @subpackage	mod_jbox_addthis
- * @copyright	Copyright (C) 20011 - 2014 JBox Web (http://www.jbox-web.com).
+ * @copyright	Copyright (C) 2014 JBox Web (http://www.jbox-web.com).
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
  */
 
+
+/**
+ * Module after install script class
+ */
 class mod_jbox_addthisInstallerScript {
-	function install($parent)
-    {
+	/**
+	 * This function is called after the module was installed : make a redirection on the modules list page
+	 * @param object $parent
+	 */
+	function install($parent){
         # $parent is the class calling this method
  		$parent->getParent()->setRedirectURL('index.php?option=com_modules');
     } 
